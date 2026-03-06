c54309a6ba82283c784c552292a2cc4aacf39711
Akshay Vishwakarma <akshay.vishwakarma@invennico.com>
2026-03-06 12:57:51 +0530

    feat: initial commit

---
+++ src/services/email.service.js
@@ -0,0 +1,38 @@
+const config = require('../config/config');
+const logger = require('../config/logger');
+const sgMail = require('@sendgrid/mail');
+
+sgMail.setApiKey(config.email.sendgrid.apiKey);
+
+/* istanbul ignore next */
+if (config.env !== 'test') {
+  logger.info('Connected to Email Server');
+}
+
+/**
+ * Send an email
+ * @param {string} to
+ * @param {string} subject
+ * @param {string} text
+ * @param {string} html
+ * @returns {Promise}
+ */
+const sendEmail = async (to, subject, text, html) => {
+  const msg = {
+    to,
+    from: config.email.sendgrid.senderMail,
+    subject,
+    text,
+    html,
+  };
+
+  try {
+    await sgMail.send(msg);
+    logger.info(`Email sent successfully to ${to}`);
+  } catch (error) {
+    logger.error(`Error sending email to ${to}: ${error.message}`);
+    throw error;
+  }
+};
+
+module.exports = { sendEmail };
